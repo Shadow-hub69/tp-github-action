1ef8a715b3f96fee805e92f541ab738c72d01e4d
Shadow-hub69 <stephaneraf12@gmail.com>
2026-04-30 15:30:53 +0300

    leo aho

---
+++ app3.js
@@ -0,0 +1,8 @@
+  GNU nano 8.3                                                                     app.js                                                                              
+function addition(a, c) {
+    return a + c;
+}
+
+console.log(addition(2, 3));
+
+
